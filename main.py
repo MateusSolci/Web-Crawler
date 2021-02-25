ba573ea385aea8c01ccef7ebe32135a474d17ca7
Mateus Solci <mateussolci@gmail.com>
2021-02-25 14:04:21 -0300

    feat: adicionado função de print dinâmico e retorno dos dados formatados em tupla

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ site_vultr = 'https://www.vultr.com/products/cloud-compute/#pricing'
 site_digital_ocean = 'https://www.digitalocean.com/pricing/#droplet'
 
 def main():
-    get_table_page1(get_page_content(site_vultr))
+    print_result(get_data_page_vultr(get_page_content(site_vultr)))
+
 
 
 
@@ -18,29 +19,39 @@ def get_page_content(url):
     return page_content
 
 
-def get_table_page1(page_content):
+def get_data_page_vultr(page_content):    
     headers = page_content.xpath('//div[@class="pt__header"]/div[contains(@class, "pt__cell") and not(contains(text(), "Geekbench Score"))]/text()')
-
     rows = page_content.xpath('//div[@class="pt__body js-body"]//div[@class="pt__row-content"]/div[contains(@class, "pt__cell")]//strong/text()')
 
-    print(headers)
-    print(rows)
+    formated_data = (headers, rows)
+
+    return formated_data
+
+
+def get_data_page_digital(page_content):
+    # busca os headers na página 1, pois, esta sessão não apresenta headers detalhados
+    headers = get_data_page_vultr()[0]
+    rows = page_content.xpath('//')
+
+    formated_data = (headers, rows)
+
+    return formated_data
+
+
+# def save_csv():
 
 
 
-
-def get_table_page2(page_content):
-    headers = page_content.xpath()
-
-    rows = page_content.xpath()
+# def save_json():
 
 
 
-def save_csv():
-
-
-
-def save_json():
+def print_result(data):
+    print(data[0])
+    for x in range(len(data[1])):
+        if x % 5 == 0:
+            print('\n')
+        print(data[1][x], end=', ')
 
 
 
